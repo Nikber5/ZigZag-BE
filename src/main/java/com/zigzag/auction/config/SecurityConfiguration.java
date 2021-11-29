ce96021efda50b1b41bf5c0a14f8c4de8ace63fd
mbereziu <mbereziu@softserveinc.com>
2021-11-29 15:24:47 +0200

    Added jwt authentication

--- src/main/java/com/zigzag/auction/config/SecurityConfiguration.java
+++ src/main/java/com/zigzag/auction/config/SecurityConfiguration.java
@@ -1,23 +1,29 @@
 package com.zigzag.auction.config;
 
+import com.zigzag.auction.jwt.JwtConfigurer;
+import com.zigzag.auction.jwt.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
-public class BasicSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final PasswordEncoder passwordEncoder;
     private final UserDetailsService userDetailsService;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public BasicSecurityConfiguration(PasswordEncoder passwordEncoder,
-                                      UserDetailsService userDetailsService) {
+    public SecurityConfiguration(PasswordEncoder passwordEncoder,
+                                 UserDetailsService userDetailsService,
+                                 JwtTokenProvider jwtTokenProvider) {
         this.passwordEncoder = passwordEncoder;
         this.userDetailsService = userDetailsService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @Autowired
@@ -28,21 +34,22 @@ public class BasicSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/index", "/lots", "/lots/*", "/users/*").permitAll()
-                .antMatchers(HttpMethod.POST, "/register").permitAll()
+                .antMatchers(HttpMethod.POST, "/register", "/login").permitAll()
                 .antMatchers(HttpMethod.GET, "/products").hasRole("USER")
                 .antMatchers(HttpMethod.POST, "/bids/*", "/lots", "/products").hasRole("USER")
                 .antMatchers(HttpMethod.PUT, "/users").hasRole("USER")
                 .antMatchers(HttpMethod.DELETE, "/users/*").hasRole("ADMIN")
                 .antMatchers(HttpMethod.GET, "/users", "/users/by-email*").hasRole("ADMIN")
-                .anyRequest()
-                .authenticated()
+                .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .permitAll()
+                .apply(new JwtConfigurer(jwtTokenProvider))
                 .and()
-                .httpBasic()
-                .and().csrf().disable();
+                .headers().frameOptions().disable();
     }
 }
--- src/main/java/com/zigzag/auction/controller/AuthenticationController.java
+++ src/main/java/com/zigzag/auction/controller/AuthenticationController.java
@@ -1,12 +1,19 @@
 package com.zigzag.auction.controller;
 
+import com.zigzag.auction.dto.request.UserLoginDto;
 import com.zigzag.auction.dto.request.UserRegisterRequestDto;
 import com.zigzag.auction.dto.response.UserResponseDto;
+import com.zigzag.auction.exception.AuthenticationException;
+import com.zigzag.auction.jwt.JwtTokenProvider;
 import com.zigzag.auction.model.User;
 import com.zigzag.auction.service.AuthenticationService;
 import com.zigzag.auction.service.mapper.RegisterMapper;
 import com.zigzag.auction.service.mapper.UserMapper;
+import java.util.Map;
+import java.util.stream.Collectors;
 import javax.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +23,15 @@ public class AuthenticationController {
     private final UserMapper mapper;
     private final RegisterMapper registerMapper;
     private final AuthenticationService authenticationService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     public AuthenticationController(UserMapper mapper, RegisterMapper registerMapper,
-                                    AuthenticationService authenticationService) {
+                                    AuthenticationService authenticationService,
+                                    JwtTokenProvider jwtTokenProvider) {
         this.mapper = mapper;
         this.registerMapper = registerMapper;
         this.authenticationService = authenticationService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @PostMapping("/register")
@@ -29,5 +39,17 @@ public class AuthenticationController {
         User user = registerMapper.mapToModel(requestDto);
         authenticationService.register(user);
         return mapper.mapToDto(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@RequestBody @Valid UserLoginDto userLoginDto)
+            throws AuthenticationException {
+        User user = authenticationService.login(userLoginDto.getLogin(),
+                userLoginDto.getPassword());
+        String token = jwtTokenProvider.createToken(user.getEmail(), user.getRoles().stream()
+                .map(role -> role.getName().name())
+                .collect(Collectors.toList()));
+        return new ResponseEntity<>(Map.of("token", token), HttpStatus.OK);
+
     }
 }
